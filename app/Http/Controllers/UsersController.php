e8b5247cfcfd5ff7193d7afc85429f150021ccc9
Solitary-y <wangyw.zt@gmail.com>
2018-07-07 01:28:42 +0000

    Add password resets & email configuration

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -116,13 +116,11 @@ class UsersController extends Controller
     {
         $view = 'emails.confirm';
         $data = compact('user');
-        $from = '623080594@qq.com';
-        $name = 'clown';
         $to = $user->email;
         $subject = '感谢注册 Sample 应用！请确认你的邮箱。';
 
-        Mail::send($view, $data, function ($message) use ($from, $name, $to, $subject) {
-            $message->from($from, $name)->to($to)->subject($subject);
+        Mail::send($view, $data, function ($message) use ($to, $subject) {
+            $message->to($to)->subject($subject);
         });
 
     }
